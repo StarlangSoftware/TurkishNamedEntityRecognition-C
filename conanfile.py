7d7b0b7615a887ba193ccc3c3922f3128c0d660d
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-10-07 17:17:50 +0300

    -

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake, tools
 
 
-class CorpusConan(ConanFile):
+class NamedEntityRecognitionConan(ConanFile):
     name = "NamedEntityRecognition-C"
     version = "1.0.0"
     requires = "Corpus-C/1.0.0"
